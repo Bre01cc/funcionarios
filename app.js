c27f73e240c5d4cca6cef3f8bf56b96999fde3af
Bre01cc <boliveiraassisreis@gmail.com>
2025-08-13 17:55:30 -0300

    javascript concluido

--- app.js
+++ app.js
@@ -9,14 +9,27 @@ function cardPerfil(funcionario){
     const galeria = document.getElementById('galeria')
 
     const perfil = document.createElement('div')
+    perfil.classList.add('perfil')
+
     const nomeTech = document.createElement('h2')
     const setorText = document.createElement('p')
     const imgPerfil = document.createElement('img')
 
-    imgPerfil.src=Funcionarios[0].nome
+    imgPerfil.src=funcionario.imagem
+    nomeTech.textContent=funcionario.nome
+    setorText.textContent =funcionario.cargo
+
+    perfil.appendChild(imgPerfil)
+    perfil.appendChild(nomeTech)
+    perfil.appendChild(setorText)
+
+    galeria.appendChild(perfil)
 
 }
-.
+function carregarDados(){
+    Funcionarios.forEach(cardPerfil)
+}
+carregarDados()
 
 
     
